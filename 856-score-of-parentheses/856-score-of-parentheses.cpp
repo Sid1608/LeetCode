b3785ac2545e7752fc33bae760a0bc7fbbbc6ea3
sid <57083596+Sid1608@users.noreply.github.com>
2022-03-17 10:48:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int scoreOfParentheses(string s) {
-        int n=s.size();
-        stack<int>st,x;
+        stack<int>st;
         int score=0;
-        unordered_map<int,int>p;
+        s="("+s+")";
+         int n=s.size();
         for(int i=0;i<n;i++){
             if(s[i]=='('){
-               
-                if(st.size())
-                    p[st.top()]++;
-                st.push(i);
+               st.push(-1);
             }else{
                 int temp=0;
-                while(p[st.top()]--){
-                    temp+=x.top();
-                    x.pop();
+                while(st.top()!=-1){
+                    temp+=st.top();
+                    st.pop();
                 }
-                if(temp==0)x.push(1);
-                else x.push(2*temp);
                 st.pop();
+                if(temp==0)st.push(1);
+                else st.push(2*temp);
             }
         }
-        while(x.size()){
-            score+=x.top();
-            x.pop();
-        }
+        score=st.top()/2;
         return score;
     }
 };
