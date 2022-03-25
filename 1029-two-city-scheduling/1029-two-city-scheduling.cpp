6c3f0a650caae625f094b093204823b86c2d33f5
sid <57083596+Sid1608@users.noreply.github.com>
2022-03-24 20:01:58 -0700

    Time: 10 ms (32.37%), Space: 9.2 MB (16.18%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    
+    //dp approach
+    //for each person we have two options either send them to city a or city b. if a ==n then we will only take b and vice versa 
+    //tc:o(n)
+    //sc:o(101*51*51)+stack
     int dp[101][51][51];
     int solve(int indx,int a,int b,vector<vector<int>>& costs,int n){
         
@@ -17,8 +22,28 @@ public:
         return dp[indx][a][b]=min(op1,op2);
     }
     int twoCitySchedCost(vector<vector<int>>& costs) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
+        // int n=costs.size();
+        // return solve(0,0,0,costs,n/2);
+        //greedy
+        //tc:o(nlogn) n:size of costs
+        //sc:o(n)
         int n=costs.size();
-        return solve(0,0,0,costs,n/2);
+        vector<pair<int,int>>diff(n);
+        for(int i=0;i<n;i++){
+            // cout<<costs[i][1]-costs[i][0]<<endl;
+            diff[i]={costs[i][1]-costs[i][0],i};
+        }
+        sort(diff.begin(),diff.end());
+        int b=0,res=0;
+        for(int i=0;i<n;i++){
+            if(b<n/2){
+                res+=costs[diff[i].second][1];
+                b++;
+            }else{
+                res+=costs[diff[i].second][0];
+            }
+        }
+        return res;
     }
 };
