9355118c6118a80f231e576bb785e971474e53c0
Siddharth Akar <57083596+Sid1608@users.noreply.github.com>
2022-08-24 22:28:56 +0530

    Time: 6 ms (21.40%), Space: 8.2 MB (98.51%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,24 +11,54 @@
  */
 class Solution {
 public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int>postorder;
+//         if(root==NULL)
+//             return postorder;
+//         stack<TreeNode*>st1,st2;
+        
+//         st1.push(root);
+//         while(!st1.empty()){
+//             TreeNode* curr=st1.top();
+//             st1.pop();
+//             st2.push(curr);
+//             if(curr->left)st1.push(curr->left);
+//             if(curr->right)st1.push(curr->right);
+//         }
+//         while(!st2.empty()){
+//             postorder.push_back(st2.top()->val);
+//             st2.pop();
+//         }
+//         return postorder;
+//     }
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int>postorder;
         if(root==NULL)
             return postorder;
-        stack<TreeNode*>st1,st2;
-        
-        st1.push(root);
-        while(!st1.empty()){
-            TreeNode* curr=st1.top();
-            st1.pop();
-            st2.push(curr);
-            if(curr->left)st1.push(curr->left);
-            if(curr->right)st1.push(curr->right);
-        }
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
+        stack<TreeNode*>st;
+        TreeNode* curr=root;
+        while(curr!=NULL or !st.empty()){
+            if(curr!=NULL){
+                st.push(curr);
+                curr=curr->left;
+            }else{
+                TreeNode* node=st.top()->right;
+                if(node==NULL){
+                    node=st.top();
+                    st.pop();
+                    postorder.push_back(node->val);
+                    while(!st.empty() and st.top()->right==node){
+                        node=st.top();
+                        postorder.push_back(node->val);
+                        st.pop();
+                    }
+                    
+                }else{
+                    curr=node;
+                }
+            }
         }
         return postorder;
+        
     }
 };
