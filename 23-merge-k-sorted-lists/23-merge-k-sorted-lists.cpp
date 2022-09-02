3e53e2d158051224c0af6209450629052973e10f
Siddharth Akar <57083596+Sid1608@users.noreply.github.com>
2022-09-02 07:44:48 +0530

    Time: 154 ms (29.55%), Space: 13.4 MB (48.40%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -10,29 +10,64 @@
  */
 class Solution {
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    
+    ListNode* mergeLists(ListNode* l1,ListNode* l2){
+        if(l1==NULL)return l2;
+        else if(l2==nullptr)return l1;
         
-        priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
-        for(auto node:lists){
-            if(node)
-               pq.push({node->val,node});
-        }
-        ListNode* head=NULL,*ptr;
-        
-        while(!pq.empty()){
-            ListNode* node=pq.top().second;
-            pq.pop();
-            if(head==NULL){
-                head=node;
-                ptr=node;
+        ListNode* ans=new ListNode(-1);
+        ListNode* ptr=ans;
+        while(l1 and l2){
+            if(l1->val<l2->val){
+                ptr->next=l1;
+                ptr=l1;
+                l1=l1->next;
             }else{
-                ptr->next=node;
-                ptr=node;
-            }
-            if(node->next!=NULL){
-                pq.push({node->next->val,node->next});
+                ptr->next=l2;
+                ptr=l2;
+                l2=l2->next;
             }
         }
-        return head;
+        if(l1){
+            ptr->next=l1;
+        }
+        if(l2){
+            ptr->next=l2;
+        }
+        return ans->next;
     }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n=lists.size();
+        if(n==0)return NULL;
+        for(int i=0;i<n-1;i++){
+            lists[i+1]=mergeLists(lists[i],lists[i+1]);
+        }
+        return lists[n-1];
+    }
+    //using min heap
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+//         priority_queue<pair<int,ListNode*>,vector<pair<int,ListNode*>>,greater<pair<int,ListNode*>>>pq;
+//         for(auto node:lists){
+//             if(node)
+//                pq.push({node->val,node});
+//         }
+//         ListNode* head=NULL,*ptr;
+        
+//         while(!pq.empty()){
+//             ListNode* node=pq.top().second;
+//             pq.pop();
+//             if(head==NULL){
+//                 head=node;
+//                 ptr=node;
+//             }else{
+//                 ptr->next=node;
+//                 ptr=node;
+//             }
+//             if(node->next!=NULL){
+//                 pq.push({node->next->val,node->next});
+//             }
+//         }
+//         return head;
+    // }
 };
